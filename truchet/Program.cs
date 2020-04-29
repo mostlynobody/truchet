02024602177646db4787ad702098c9ef4156fa36
Matthias <matthias.steinletzberger@gmail.com>
2020-04-29 22:26:58 +0200

    Generator using perlin noise v0.00001

--- truchet/Program.cs
+++ truchet/Program.cs
@@ -3,64 +3,80 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using Truchet.Tiles;
+using Truchet.Perlin;
 
 namespace Truchet
 {
     class Program
     {
+        //compile time variables
+        private const int TILE_SIZE = 200;
+        private const int TILE_ROWS = 20;
+        private const int TILE_COLUMNS = 40;
 
-        private static readonly int TILE_SIZE = 120;
-        private static readonly int TILE_ROWS = 100;
-        private static readonly int TILE_COLUMNS = 200;
+        private const int DIVISION_LEVELS = 3;
 
-        private static readonly int DIVISION_LEVELS = 2;
+        private const int SEED = 234243;
 
-        private static readonly int SEED = 123019491;
-        private static readonly int PRIMARY = 0xFFFFFF;
-        private static readonly int SECONDARY = 0x000000;
+        private const int PRIMARY = 0xA6C36F;
+        private const int SECONDARY = 0x335145;
+
+        private const bool BORDERLESS = true;
+
+
         private static readonly Tileset TILESET = new Tileset(TILE_SIZE, DIVISION_LEVELS, PRIMARY, SECONDARY);
-        private static readonly Random RANDOM = new Random(1234);
+        private static readonly Random RANDOM = new Random(SEED);
 
-        private static readonly bool BORDERLESS = false;
+
+
+        
 
         static void Main(string[] args)
         {
-
+            // initialize all variables that are not done at compile time;
             int canvas_width = (TILE_COLUMNS) * TILE_SIZE;
             int canvas_height = (TILE_ROWS) * TILE_SIZE;
+
             if(!BORDERLESS)
             {
                 canvas_width += TILE_SIZE;
                 canvas_height += TILE_SIZE;
             }
 
+
             Image canvas = new Bitmap(canvas_width, canvas_height, PixelFormat.Format32bppArgb);
             Graphics grp = Graphics.FromImage(canvas);
 
-            //block matrix for the tiles
-            //var tiles = new Queue<Tile>();
-            var tiles = createRandomDebugQueue();
+            var noise = NoiseMap.GenerateNoiseMap(RANDOM, canvas_width / 10, canvas_height / 10, 2d, 1d, 3);
 
-            //MAIN LOOP FOR DRAWING
-
-
+            generateNoiseDebugImage(noise);
             TILESET.GenerateDebugImage();
+
+           
+            //block matrix for the tiles
+            Tile[,] tileMatrix = createPerlinTileMatrix(noise);
+
+            var tileQueue = new Queue<Tile>();
+            foreach (Tile t in tileMatrix) tileQueue.Enqueue(t);
+             
             for (int currentLevel = 0; currentLevel < DIVISION_LEVELS; currentLevel ++)
             {
-                var subTiles = new Queue<Tile>();
 
-                //since the images are actually bigger, they need to be offset by an increasing ammount
-                //the deeper we get into the division levels
-                //each level above 0 gains TileSize/(2^(level+1) offset
-                //  if (currentLevel == 1) offset = TILE_SIZE / 4;
-                // else if (currentLevel > 1) offset +=  2;
-                //offset is also used if borderlesss is on.
+                var subTileQueue = new Queue<Tile>();
+
+                /*ince the images are actually bigger, they need to be offset 
+                 * by an increasing ammount
+                the deeper we get into the division levels
+                each level above 0 gains TileSize/(2^(level+1) offset
+                offset is also used if borderlesss is on. */
                 int offset = 0;
+
                 if (BORDERLESS) offset -= TILE_SIZE / 2;
                 if (currentLevel > 0)
                 {
-                    offset += TILE_SIZE / 4;
-                    int temp = offset;
+                    int temp = TILE_SIZE / 4;
+                    offset += temp;
                     for (int pow = 1; pow < currentLevel; pow++)
                     {
                         temp /= 2;
@@ -69,13 +85,13 @@ namespace Truchet
                 }
  
 
-                foreach (Tile t in tiles)
+                foreach (Tile t in tileQueue)
                 {
                     if(t.type == TileType.Container) {
                         var ct = (ContainerTile)t;
                         foreach(Tile child in ct.container)
                         {
-                            subTiles.Enqueue(child);
+                            subTileQueue.Enqueue(child);
                         }
                     }
                     else
@@ -84,24 +100,37 @@ namespace Truchet
                         grp.DrawImage(gt.image, gt.x+offset, gt.y+offset);
                     }
                 }
-                tiles = subTiles;
+                tileQueue = subTileQueue;
             }
             
 
             canvas.Save("test.png", ImageFormat.Png);
         }
 
-        private static Queue<Tile> createRandomDebugQueue()
+        private static Tile[,] createPseudorandomTileMatrix()
         {
-            Queue<Tile> q = new Queue<Tile>();
+            Tile[,] t = new Tile[TILE_ROWS, TILE_COLUMNS];
             for(int x = 0; x < TILE_COLUMNS; x++)
             {
                 for (int y = 0; y < TILE_ROWS; y++)
                 {
-                    q.Enqueue(generateRandomTile(1, x*TILE_SIZE, y*TILE_SIZE));
+                    t[y,x] = (generateRandomTile(1, x*TILE_SIZE, y*TILE_SIZE));
                 }
             }
-            return q;
+            return t;
+        }
+
+        private static Tile[,] createPerlinTileMatrix(double [,] noise)
+        {
+            Tile[,] t = new Tile[TILE_ROWS, TILE_COLUMNS];
+            for (int x = 0; x < TILE_COLUMNS; x++)
+            {
+                for (int y = 0; y < TILE_ROWS; y++)
+                {
+                    t[y, x] = generateRandomPerlinTile(1, x * TILE_SIZE, y * TILE_SIZE, noise);
+                }
+            }
+            return t;
         }
 
         private static Tile generateRandomTile(int level, int x, int y)
@@ -120,10 +149,73 @@ namespace Truchet
             }
             else
             {
-                int randomType = RANDOM.Next(14);
+                //int randomType = RANDOM.Next(TILESET.tileCount-1);
+                int randomType = 1 + RANDOM.Next(13);
                 t = new GraphicTile(x, y, level, (TileType)randomType, TILESET.GetTile(level-1, randomType));
             }
             return t;
+        }
+
+        private static Tile generateRandomPerlinTile(int level, int x, int y, double[,] noise)
+        {
+            Tile t;
+            //NOISE MAL ZEHN DU WEI?T DEI NOISE IS UM EIN FAKTOR 10 KLEINER ALS DIE EIGENTLICHE DINGS ALSO DU WEIßt WAS ICH MEINE OK
+            // OK DANN IST JA GUT :)))
+            double noiseValue = noise[x / 10, y / 10];
+
+            //DEBUG
+            double limit1 = 0.5d;
+            double risinglimit = 0.10;
+            //LIL BIT OF RANDOMNESS
+            noiseValue += (RANDOM.NextDouble() - 0.5) / 3;
+
+            bool isContainer = false;
+            if (noiseValue < limit1 - ((level-1) * (risinglimit))) isContainer = true;
+            if (level < DIVISION_LEVELS && isContainer)
+            {   
+                //NW; NE; SE; SW
+                Tile[] subdivision = new Tile[4];
+                int offset = TILE_SIZE / (Convert.ToInt32(Math.Pow(2, level)));
+                subdivision[0] = generateRandomPerlinTile(level + 1, x, y, noise);
+                subdivision[1] = generateRandomPerlinTile(level + 1, x + offset, y, noise);
+                subdivision[2] = generateRandomPerlinTile(level + 1, x + offset, y + offset, noise);
+                subdivision[3] = generateRandomPerlinTile(level + 1, x, y + offset, noise);
+                t = new ContainerTile(x, y, level, subdivision);
+            }
+            else
+            {
+                //int randomType = RANDOM.Next(TILESET.tileCount-1);
+                int randomType = 1 + RANDOM.Next(13);
+                t = new GraphicTile(x, y, level, (TileType)randomType, TILESET.GetTile(level - 1, randomType));
+            }
+            return t;
+        }
+
+
+
+        private static void generateNoiseDebugImage(double[,] noiseMatrix)
+        {
+            int width = noiseMatrix.GetLength(0);
+            int height = noiseMatrix.GetLength(1);
+            Brush[] greyscale = new Brush[256];
+            for(int i = 0; i < 256; i++)
+            {
+                int rgb = (((i << 8) + i) << 8) + i;
+                greyscale[i] = new SolidBrush(Color.FromArgb((0xFF << 24) ^ rgb));
+            }
+
+            Image img = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(img);
+            for(int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    int xf = (int)Math.Floor(255 * noiseMatrix[x, y]);
+                    g.FillRectangle(greyscale[xf], x, y, 1, 1);
+                }
+            }
+
+            img.Save("perlindebug.png", ImageFormat.Png);
         }
     }
 
