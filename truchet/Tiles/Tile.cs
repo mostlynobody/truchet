c769182a172b2aa736981e7404d48ae643d07efd
Matthias <matthias.steinletzberger@gmail.com>
2020-05-11 21:53:11 +0200

    Palettes and a start at connected tiles

--- truchet/Tiles/Tile.cs
+++ truchet/Tiles/Tile.cs
@@ -6,43 +6,43 @@ namespace Truchet.Tiles
 
     abstract class Tile
     {
-        public int x { get; }
-        public int y { get; }
-        public int level { get; }
-        public bool isContainer { get; }
+        public int X { get; }
+        public int Y { get; }
+        public int Level { get; }
+        public bool IsContainer { get; }
 
         public Tile(int x, int y, int level, bool isContainer)
         {
-            this.x = x;
-            this.y = y;
-            this.level = level;
-            this.isContainer = isContainer;
+            this.X = x;
+            this.Y = y;
+            this.Level = level;
+            this.IsContainer = isContainer;
         }
     }
 
     class ContainerTile : Tile
     {
         //Clockwise from NW: NW, NE, SE, SW, 
-        public Tile[] container { get; }
+        public Tile[] Container { get; }
 
         public ContainerTile(int x, int y, int level, Tile[] subdivison) 
             : base(x, y, level, true)
         {
             foreach (Tile t in subdivison) if (t == null) throw new Exception("container has to be filled");
-            container = subdivison;
+            Container = subdivison;
         }
     }
 
     class GraphicTile : Tile
     {
         //Reference to the tileset img
-        public Image image { get; }
-        public TileType type { get; }
+        public Image Image { get; }
+        public TileType Type { get; }
         public GraphicTile(int x, int y, int level, TileType type, Image image) 
             : base(x, y, level, false)
         {
-            this.type = type;
-            this.image = image;
+            Type = type;
+            Image = image;
         }
     }
 }
