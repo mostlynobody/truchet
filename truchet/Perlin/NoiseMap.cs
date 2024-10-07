a4e0e1bb6b8eaf66d1226f5f7431ccdac2b943ae
mostlynobody <32034333+mostlynobody@users.noreply.github.com>
2024-10-07 14:35:24 +0200

    perlin noise

---
+++ truchet/Perlin/NoiseMap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Truchet.Perlin
+{
+    class NoiseMap
+    {   
+
+        public static double[,] GenerateNoiseMap(Random rand, int width, int height,  double frequency,  double amplitude, int octaves)
+        {
+            PerlinNoise perlin = new PerlinNoise(rand);
+            double[,] matrix = new double[width, height];
+
+            /// track min and max noise value. Used to normalize the result to the 0 to 1.0 range.
+            double min = double.MaxValue;
+            double max = double.MinValue;
+            
+
+
+            for (var octave = 0; octave < octaves; octave++)
+            {
+                Parallel.For(0, width, (x) =>
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        double noise = perlin.Noise(x * frequency * 1d / width, y * frequency * 1d / height);
+                        noise *= amplitude;
+                        matrix[x, y] = noise;
+                        min = Math.Min(min, noise);
+                        max = Math.Max(max, noise);
+                    }
+                }
+                );
+                frequency *= 2;
+                amplitude /= 2;
+            }
+
+            //normalize matrix
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    matrix[x, y] = (matrix[x, y] - min) / (max - min);
+            return matrix;
+        }
+    }
+}
--- truchet/Perlin/Perlin.cs
+++ truchet/Perlin/Perlin.cs
@@ -5,16 +5,14 @@
 /// http://www.siafoo.net/snippet/144?nolinenos#perlin2003
 
 using System;
-using System.Threading.Tasks;
 
 namespace Truchet.Perlin
 {
 
-    class PerlinNoise
+    internal class PerlinNoise
     {
-        //always use the same random generator
-        private Random rand;
 
+        private Random rand;
         private int[] permutation;
         private Vec2[] gradients;
 
@@ -70,7 +68,7 @@ namespace Truchet.Perlin
                 new Vec2(cell.x+1, cell.y+1) 
             };
 
-            double total = 0;
+            double sum = 0;
             foreach (Vec2 v in corners)
             {
                 Vec2 w = origin - v;
@@ -78,9 +76,9 @@ namespace Truchet.Perlin
                 i = permutation[(i + (int)v.y) % SIZE];
                 Vec2 grad = gradients[i % SIZE];
 
-                total += Smoothstep(w.x, w.y) * Vec2.DotProduct(grad, w);
+                sum += Smoothstep(w.x, w.y) * Vec2.DotProduct(grad, w);
             }
-            return Math.Max(Math.Min(total, 1d), -1d);
+            return Math.Max(Math.Min(sum, 1d), -1d);
         }
 
         private double Smoothstep(double a, double b)
@@ -90,42 +88,6 @@ namespace Truchet.Perlin
             a = 1d - a * a * a * (a * (a * 6 - 15) + 10);
             b = 1d - b * b * b * (b * (b * 6 - 15) + 10);
             return a * b;
-        }
-
-        public double[,] GenerateNoiseMap(int width, int height, double frequency, double amplitude, int octaves)
-        {
-            double[,]matrix = new double[width, height];
-
-            double min = double.MaxValue;
-            double max = double.MinValue;
-
-            for (var octave = 0; octave < octaves; octave++)
-            {
-                Parallel.For(0, width, (x) =>
-                    {
-                        for (int y = 0; y < height; y++)
-                        {
-                            double noise = Noise(x * frequency * 1d / width,
-                                                 y * frequency * 1d / height);
-                            noise *= amplitude;
-                            matrix[x, y] = noise;
-                            min = Math.Min(min, noise);
-                            max = Math.Max(max, noise);
-                        }
-                    }
-                );
-
-                frequency *= 2;
-                amplitude /= 2;
-            }
-            for (int x = 0; x < width; x++) 
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    matrix[x, y] = (matrix[x, y] - min) / (max - min);
-                }
-            }
-            return matrix;
         }
     }
 }
