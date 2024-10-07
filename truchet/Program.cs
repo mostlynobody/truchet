8b399c14d4922fb5c83fd7b47b6eea31615aae60
mostlynobody <32034333+mostlynobody@users.noreply.github.com>
2024-10-07 14:35:24 +0200

    first version complete

--- truchet/Program.cs
+++ truchet/Program.cs
@@ -9,13 +9,14 @@ namespace Truchet
     class Program
     {
 
-        private static readonly int TILE_SIZE = 600;
+        private static readonly int TILE_SIZE = 360;
         private static readonly int TILE_ROWS = 10;
         private static readonly int TILE_COLUMNS = 10;
 
-        private static readonly int DIVISION_LEVELS = 4;
+        private static readonly int DIVISION_LEVELS = 3;
 
         private static readonly int SEED = 123019491;
+        private static readonly Tileset TILESET = new Tileset(TILE_SIZE, DIVISION_LEVELS, 0xFFFFFF, 0x000000);
         private static readonly Random RANDOM = new Random(SEED);
 
         static void Main(string[] args)
@@ -27,32 +28,26 @@ namespace Truchet
             Graphics grp = Graphics.FromImage(canvas);
 
             //block matrix for the tiles
-            var tiles = new Queue<Tile>();
+            //var tiles = new Queue<Tile>();
+            var tiles = createRandomDebugQueue();
 
             //MAIN LOOP FOR DRAWING
-            int offset = 0;
 
 
-            Tileset tileset = new Tileset(TILE_SIZE, DIVISION_LEVELS, 0xFFFFFF, 0x000000);
-            tileset.GenerateDebugImage();
-
-            /*
+            TILESET.GenerateDebugImage();
             for (int currentLevel = 0; currentLevel < DIVISION_LEVELS; currentLevel ++)
             {
                 var subTiles = new Queue<Tile>();
-               
+
                 //since the images are actually bigger, they need to be offset by an increasing ammount
                 //the deeper we get into the division levels
                 //each level above 0 gains TileSize/(2^(level+1) offset
-                            
-                if(currentLevel == 1)
-                {
-                    offset = TILE_SIZE / 4;
-                }
-                else if(currentLevel > 1)
-                {
-                    offset /= 2;
-                }
+                //  if (currentLevel == 1) offset = TILE_SIZE / 4;
+                // else if (currentLevel > 1) offset +=  2;
+                int offset = 0;
+                if (currentLevel > 0)  offset = TILE_SIZE / 4;
+                for (int pow = 1; pow < currentLevel; pow++) offset += offset / 2;
+ 
 
                 foreach (Tile t in tiles)
                 {
@@ -66,14 +61,51 @@ namespace Truchet
                     else
                     {
                         var gt = (GraphicTile)t;
-                        grp.DrawImage(gt.image, gt.x, gt.y);
+                        grp.DrawImage(gt.image, gt.x+offset, gt.y+offset);
                     }
                 }
                 tiles = subTiles;
             }
-            */
+            
 
-            //canvas.Save("test.png", ImageFormat.Png);
+            canvas.Save("test.png", ImageFormat.Png);
+        }
+
+        private static Queue<Tile> createRandomDebugQueue()
+        {
+            Queue<Tile> q = new Queue<Tile>();
+            for(int x = 0; x < TILE_COLUMNS; x++)
+            {
+                for (int y = 0; y < TILE_ROWS; y++)
+                {
+                    q.Enqueue(generateRandomTile(1, x*TILE_SIZE, y*TILE_SIZE));
+                }
+            }
+            return q;
+        }
+
+        private static Tile generateRandomTile(int level, int x, int y)
+        {
+            Tile t;
+            if (level < DIVISION_LEVELS && RANDOM.Next(level+1) == 0)
+            {
+                //NW; NE; SE; SW
+                Tile[] subdivision = new Tile[4];
+                int offset = TILE_SIZE / (Convert.ToInt32(Math.Pow(2, level)));
+                subdivision[0] = generateRandomTile(level + 1, x, y);
+                subdivision[1] = generateRandomTile(level + 1, x + offset, y);
+                subdivision[2] = generateRandomTile(level + 1, x + offset, y + offset);
+                subdivision[3] = generateRandomTile(level + 1, x, y + offset);
+                t = new ContainerTile(x, y, level, subdivision);
+            }
+            else
+            {
+                int randomType = RANDOM.Next(14);
+                t = new GraphicTile(x, y, level, (TileType)randomType, TILESET.GetTile(level-1, randomType));
+            }
+            return t;
         }
     }
+
+
 }
